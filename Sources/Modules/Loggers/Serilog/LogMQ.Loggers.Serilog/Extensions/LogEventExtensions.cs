4791119b8c68a5cf9d037fd7dcf25ee1b82270d0
b-erik99 <ebarale.develop@gmail.com>
2024-11-22 15:45:49 +0100

    fixed bug on wrong metadata in logs moving metadata provider to the logger scope

--- Sources/Modules/Loggers/Serilog/LogMQ.Loggers.Serilog/Extensions/LogEventExtensions.cs
+++ Sources/Modules/Loggers/Serilog/LogMQ.Loggers.Serilog/Extensions/LogEventExtensions.cs
@@ -1,45 +1,84 @@
 ﻿using LogMQ.Core;
+using Serilog;
 using Serilog.Events;
 using Serilog.Parsing;
+using System.Diagnostics;
 
 namespace LogMQ.Loggers.Serilog.Extensions;
 
 internal static class LogEventExtensions
 {
-	internal static LogMessage ToLogMessage(this LogEvent logEvent, IFormatProvider formatProvider, string applicationName, string category) => new()
-	{
-		Timestamp = logEvent.Timestamp,
-		LogLevel = logEvent.Level.ToLogMQLogLevel(),
-		Message = logEvent.RenderMessage(formatProvider),
-		Application = new LogApplication()
-		{
-			Name = applicationName,
-			Category = category,
-			Machine = Environment.MachineName,
-			Pid = Environment.ProcessId,
-		},
-		Meta = LogMetadata.GetMetadata(logEvent.Exception)
-	};
+    internal static LogMessage ToLogMessage(this LogEvent logEvent, IFormatProvider formatProvider, string applicationName, string category) => new()
+    {
+        Timestamp = logEvent.Timestamp,
+        LogLevel = logEvent.Level.ToLogMQLogLevel(),
+        Message = logEvent.RenderMessage(formatProvider),
+        Application = new LogApplication()
+        {
+            Name = applicationName,
+            Category = category,
+            Machine = Environment.MachineName,
+            Pid = Environment.ProcessId,
+        },
+        Meta = logEvent.GetMetadata()
+    };
 
-	internal static LogEvent ToLogEvent(this LogMessage logMessage)
-	{
-		var serilogLevel = logMessage.LogLevel.ToSerilogLogLevel();
-		var messageTemplate = new MessageTemplateParser().Parse(logMessage.Message ?? string.Empty);
-		var exception = new Exception(logMessage.Meta?.Exception);
-		var properties = new List<LogEventProperty>
-		{
-			new(nameof(LogApplication.Name), new ScalarValue(logMessage.Application.Name)),
-			new(nameof(LogApplication.Category), new ScalarValue(logMessage.Application.Category)),
-			new(nameof(LogApplication.Machine), new ScalarValue(logMessage.Application.Machine)),
-			new(nameof(LogApplication.Pid), new ScalarValue(logMessage.Application.Pid)),
-			new(nameof(LogMetadata), new ScalarValue(logMessage.Meta))
-		};
-		return new LogEvent(
-			logMessage.Timestamp,
-			serilogLevel,
-			exception,
-			messageTemplate,
-			properties
-		);
-	}
+    /// <summary>
+    /// Extracts metadata from the current stack trace and an optional log event exception.
+    /// </summary>
+    /// <param name="logEvent">The log event containing information about the current log entry, including an optional exception.</param>
+    /// <returns>
+    /// A <see cref="LogMetadata"/> object populated with details about the class, method, method signature,
+    /// file, line number, and exception (if present).
+    /// </returns>
+    /// <remarks>
+    /// The method analyzes the stack trace to identify the calling method, skipping frames related to the logging assembly
+    /// (<see cref="Log"/>). It also supports asynchronous methods and lambda functions, including their signatures.
+    /// </remarks>
+    private static LogMetadata GetMetadata(this LogEvent logEvent)
+    {
+        var logMetadata = new LogMetadata();
+        var frame = EnhancedStackTrace.Current().Take(128).Skip(3)
+                          .FirstOrDefault(f => f.HasMethod() && f.MethodInfo.DeclaringType?.Assembly != typeof(Log).Assembly);
+
+        if (frame == null) return logMetadata;
+
+        var methodInfo = frame.MethodInfo;
+        logMetadata.Class = methodInfo.DeclaringType?.FullName;
+        logMetadata.MethodName = methodInfo.Name;
+        logMetadata.MethodSignature = methodInfo.IsLambda ? "Lambda [" : "";
+        logMetadata.MethodSignature += $"{(methodInfo.IsAsync ? "Async" : "")} {methodInfo.ReturnParameter?.ResolvedType?.FullName ?? "void"} {methodInfo.Name}";
+        var parameters = methodInfo.Parameters;
+        logMetadata.MethodSignature += parameters.Count > 0
+            ? $"({string.Join(", ", parameters.Select(p => $"{p.ResolvedType.FullName} {p.Name}"))})"
+            : "()";
+        logMetadata.MethodSignature += methodInfo.IsLambda ? "]" : "";
+        logMetadata.File = frame.GetFileName();
+        logMetadata.Row = frame.GetFileLineNumber();
+        logMetadata.Exception = (logEvent.Exception?.ToString());
+
+        return logMetadata;
+    }
+
+    internal static LogEvent ToLogEvent(this LogMessage logMessage)
+    {
+        var serilogLevel = logMessage.LogLevel.ToSerilogLogLevel();
+        var messageTemplate = new MessageTemplateParser().Parse(logMessage.Message ?? string.Empty);
+        var exception = new Exception(logMessage.Meta?.Exception);
+        var properties = new List<LogEventProperty>
+        {
+            new(nameof(LogApplication.Name), new ScalarValue(logMessage.Application.Name)),
+            new(nameof(LogApplication.Category), new ScalarValue(logMessage.Application.Category)),
+            new(nameof(LogApplication.Machine), new ScalarValue(logMessage.Application.Machine)),
+            new(nameof(LogApplication.Pid), new ScalarValue(logMessage.Application.Pid)),
+            new(nameof(LogMetadata), new ScalarValue(logMessage.Meta))
+        };
+        return new LogEvent(
+            logMessage.Timestamp,
+            serilogLevel,
+            exception,
+            messageTemplate,
+            properties
+        );
+    }
 }
