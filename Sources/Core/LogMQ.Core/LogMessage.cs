9c06cefb847c4557777c1a2d131cdede4f294027
Erik Barale <ebarale.develop@gmail.com>
2024-11-26 10:53:15 +0100

    code refactoring

--- Sources/Core/LogMQ.Core/LogMessage.cs
+++ Sources/Core/LogMQ.Core/LogMessage.cs
@@ -8,83 +8,83 @@ namespace LogMQ.Core;
 [ProtoContract]
 public class LogMessage : IEquatable<LogMessage>
 {
-    /// <summary>
-    /// Gets or sets the unique id of the log message.
-    /// </summary>
-    [ProtoMember(1)]
-    public Guid Guid { get; set; }
+	/// <summary>
+	/// Gets or sets the unique id of the log message.
+	/// </summary>
+	[ProtoMember(1)]
+	public Guid Guid { get; set; }
 
-    /// <summary>
-    /// Gets or sets the timestamp of when the log event occurred.
-    /// </summary>
-    public DateTimeOffset Timestamp
-    {
-        get => new(timestampDateTime, timestampOffset);
-        set
-        {
-            timestampDateTime = value.DateTime;
-            timestampOffset = value.Offset;
-        }
-    }
+	/// <summary>
+	/// Gets or sets the timestamp of when the log event occurred.
+	/// </summary>
+	public DateTimeOffset Timestamp
+	{
+		get => new(timestampDateTime, timestampOffset);
+		set
+		{
+			timestampDateTime = value.DateTime;
+			timestampOffset = value.Offset;
+		}
+	}
 
-    /// <summary>
-    /// Gets or sets the severity level of the log message.
-    /// </summary>
-    [ProtoMember(2)]
-    public LogLevel LogLevel { get; set; }
+	/// <summary>
+	/// Gets or sets the severity level of the log message.
+	/// </summary>
+	[ProtoMember(2)]
+	public LogLevel LogLevel { get; set; }
 
-    /// <summary>
-    /// Gets or sets the actual log message describing the event or action.
-    /// </summary>
-    [ProtoMember(3)]
-    public string Message { get; set; }
+	/// <summary>
+	/// Gets or sets the actual log message describing the event or action.
+	/// </summary>
+	[ProtoMember(3)]
+	public string Message { get; set; }
 
-    /// <summary>
-    /// Gets or sets the application generating the log message.
-    /// </summary>
-    [ProtoMember(4)]
-    public LogApplication Application { get; set; }
+	/// <summary>
+	/// Gets or sets the application generating the log message.
+	/// </summary>
+	[ProtoMember(4)]
+	public LogApplication Application { get; set; }
 
-    /// <summary>
-    /// Gets or sets additional metadata related to the log event, such as file, class, and method information.
-    /// </summary>
-    [ProtoMember(5)]
-    public LogMetadata Meta { get; set; }
+	/// <summary>
+	/// Gets or sets additional metadata related to the log event, such as file, class, and method information.
+	/// </summary>
+	[ProtoMember(5)]
+	public LogMetadata Meta { get; set; }
 
-    [ProtoMember(6)]
-    private DateTime timestampDateTime;
+	[ProtoMember(6)]
+	private DateTime timestampDateTime;
 
-    [ProtoMember(7)]
-    private TimeSpan timestampOffset;
+	[ProtoMember(7)]
+	private TimeSpan timestampOffset;
 
-    /// <summary>
-    /// Serializes the log message to the specified stream using Protocol Buffers (protobuf).
-    /// </summary>
-    /// <param name="stream">The stream to which the log message will be serialized.</param>
-    /// <returns>The same stream that was passed in.</returns>
-    public Stream SerializeToStream(Stream stream)
-    {
-        Serializer.Serialize(stream, this);
-        return stream;
-    }
+	/// <summary>
+	/// Serializes the log message to the specified stream using Protocol Buffers (protobuf).
+	/// </summary>
+	/// <param name="stream">The stream to which the log message will be serialized.</param>
+	/// <returns>The same stream that was passed in.</returns>
+	public Stream SerializeToStream(Stream stream)
+	{
+		Serializer.Serialize(stream, this);
+		return stream;
+	}
 
-    /// <summary>
-    /// Serializes the log message and returns it as a byte array using Protocol Buffers (protobuf).
-    /// </summary>
-    /// <returns>A byte array containing the serialized log message.</returns>
-    public byte[] Serialize()
-    {
-        using MemoryStream stream = new();
-        SerializeToStream(stream);
-        return stream.ToArray();
-    }
+	/// <summary>
+	/// Serializes the log message and returns it as a byte array using Protocol Buffers (protobuf).
+	/// </summary>
+	/// <returns>A byte array containing the serialized log message.</returns>
+	public byte[] Serialize()
+	{
+		using MemoryStream stream = new();
+		SerializeToStream(stream);
+		return stream.ToArray();
+	}
 
-    /// <summary>
-    /// Deserializes a log message from the specified stream using Protocol Buffers (protobuf).
-    /// </summary>
-    /// <param name="stream">The stream from which the log message will be deserialized.</param>
-    /// <returns>The deserialized log message.</returns>
-    public static LogMessage Deserialize(Stream stream) => Serializer.Deserialize<LogMessage>(stream);
+	/// <summary>
+	/// Deserializes a log message from the specified stream using Protocol Buffers (protobuf).
+	/// </summary>
+	/// <param name="stream">The stream from which the log message will be deserialized.</param>
+	/// <returns>The deserialized log message.</returns>
+	public static LogMessage Deserialize(Stream stream) => Serializer.Deserialize<LogMessage>(stream);
 
 	/// <inheritdoc/>
 	public override bool Equals(object obj) => Equals(obj as LogMessage);
@@ -97,4 +97,7 @@ public class LogMessage : IEquatable<LogMessage>
 
 	/// <inheritdoc/>
 	public static bool operator !=(LogMessage left, LogMessage right) => !(left == right);
+
+	/// <inheritdoc/>
+	public override int GetHashCode() => HashCode.Combine(Guid);
 }
--- Sources/Modules/Loggers/Serilog/LogMQ.Loggers.Serilog/LogMQEnricher.cs
+++ Sources/Modules/Loggers/Serilog/LogMQ.Loggers.Serilog/LogMQEnricher.cs
@@ -21,22 +21,22 @@ internal sealed class LogMQEnricher : ILogEventEnricher
 	/// A factory for creating structured properties to attach to the log event.
 	/// </param>
 	public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
-    {
-        var meta = logEvent.GetMetadata();
+	{
+		var meta = logEvent.GetMetadata();
 
-        var properties = new[]
-{
-            propertyFactory.CreateProperty(nameof(meta.File), meta.File),
-            propertyFactory.CreateProperty(nameof(meta.Class), meta.Class),
-            propertyFactory.CreateProperty(nameof(meta.MethodName), meta.MethodName),
-            propertyFactory.CreateProperty(nameof(meta.MethodSignature), meta.MethodSignature),
-            propertyFactory.CreateProperty(nameof(meta.Row), meta.Row),
-            propertyFactory.CreateProperty(nameof(meta.Exception), meta.Exception)
-        };
+		var properties = new[]
+		{
+			propertyFactory.CreateProperty(nameof(meta.File), meta.File),
+			propertyFactory.CreateProperty(nameof(meta.Class), meta.Class),
+			propertyFactory.CreateProperty(nameof(meta.MethodName), meta.MethodName),
+			propertyFactory.CreateProperty(nameof(meta.MethodSignature), meta.MethodSignature),
+			propertyFactory.CreateProperty(nameof(meta.Row), meta.Row),
+			propertyFactory.CreateProperty(nameof(meta.Exception), meta.Exception)
+		};
 
-        foreach (var property in properties)
-        {
-            logEvent.AddPropertyIfAbsent(property);
-        }
-    }
+		foreach (var property in properties)
+		{
+			logEvent.AddPropertyIfAbsent(property);
+		}
+	}
 }
