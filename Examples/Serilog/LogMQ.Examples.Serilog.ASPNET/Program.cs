937c725a18876f5e3191921ea5be22e32b6155ad
Erik Barale <ebarale.develop@gmail.com>
2024-11-27 14:23:34 +0100

    added ASPNET example for serilog

---
+++ Examples/Serilog/LogMQ.Examples.Serilog.ASPNET/Program.cs
@@ -0,0 +1,34 @@
+using LogMQ.Loggers.Serilog;
+using LogMQ.Providers;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+	.MinimumLevel.Verbose()
+	.UseLogMQStack(new TcpProvider(null, "localhost", 5563, new FallbackLogger()), "ConsoleApplication", "TCP_PROVIDER")
+	.WriteTo.Console()
+	.CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Services.AddSerilog();
+builder.Services.AddSingleton<ExampleHandler>();
+
+var app = builder.Build();
+
+var handler = app.Services.GetRequiredService<ExampleHandler>();
+app.MapGet("/", handler.HandleRequest);
+
+await app.RunAsync();
+
+partial class ExampleHandler(ILogger<ExampleHandler> logger)
+{
+	public string HandleRequest()
+	{
+		LogHandleRequest(logger);
+		return "Hello World";
+	}
+
+	[LoggerMessage(LogLevel.Information, "ExampleHandler.HandleRequest was called")]
+	public static partial void LogHandleRequest(Microsoft.Extensions.Logging.ILogger logger);
+}
