61dcbeeaf69bb4e66e1dc6c85b3b7f7c31396fd8
b-erik99 <ebarale.develop@gmail.com>
2024-12-02 13:24:50 +0100

    updated cli interface implementation

--- Sources/Services/Shared/LogMQ.Services.Shared.PluginManager/PluginManager.cs
+++ Sources/Services/Shared/LogMQ.Services.Shared.PluginManager/PluginManager.cs
@@ -3,16 +3,18 @@ using System.Text.Json;
 
 namespace LogMQ.Services.Shared.PluginManager;
 
-public class PluginManager //: IPluginManager
+public static class PluginManager //: IPluginManager
 {
     public static readonly string PluginFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "LogMQ", "Plugins");
 
     public static readonly string PluginConfigFile = Path.Combine(PluginFolder, "pluginconfig.json");
+    public static readonly string PluginConfigBackupFile = $"{PluginConfigFile}.bak";
 
     public static readonly string PluginBinariesFolder = Path.Combine(PluginFolder, "Binaries");
 
+    //Plugin management
 
-    private static async Task<List<PluginConfig>> LoadPluginsConfig(FileStream fileStream)
+    private static async Task<List<PluginConfig>> LoadPluginsConfigAsync(FileStream fileStream)
     {
         using StreamReader reader = new(fileStream);
         fileStream.SetLength(0);
@@ -34,7 +36,7 @@ public class PluginManager //: IPluginManager
     public static async Task<PluginConfig> DisablePluginAsync(Guid pluginId, Version version = null)
     {
         await using FileStream fileStream = new(PluginConfigFile, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
-        List<PluginConfig> plugins = await LoadPluginsConfig(fileStream);
+        List<PluginConfig> plugins = await LoadPluginsConfigAsync(fileStream);
         PluginConfig plugin = plugins.Find(p => p.Id == pluginId);
         if (plugin?.ActiveVersion != null && (version == null || plugin.ActiveVersion.Version == version))
         {
@@ -48,7 +50,7 @@ public class PluginManager //: IPluginManager
     public static async Task<PluginConfig> EnablePluginAsync(Guid pluginId, Version version = null)
     {
         await using FileStream fileStream = new(PluginConfigFile, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
-        List<PluginConfig> plugins = await LoadPluginsConfig(fileStream);
+        List<PluginConfig> plugins = await LoadPluginsConfigAsync(fileStream);
         PluginConfig plugin = plugins.Find(p => p.Id == pluginId);
         if (plugin != null)
         {
@@ -59,47 +61,102 @@ public class PluginManager //: IPluginManager
         return plugin;
     }
 
-    public Task<string> GetBrokerStatusAsync()
+    public static async Task<PluginConfig> InstallPluginAsync(string pluginPath)
+    {
+        throw new NotImplementedException();
+    }
+    public static async Task<PluginConfig> UninstallPluginAsync(Guid pluginId, Version version = null)
+    {
+        await using FileStream fileStream = new(PluginConfigFile, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+        List<PluginConfig> plugins = await LoadPluginsConfigAsync(fileStream);
+        PluginConfig plugin = plugins.Find(p => p.Id == pluginId);
+        if (plugin?.ActiveVersion != null && (version == null || plugin.ActiveVersion.Version == version))
+        {
+            plugin.ActiveVersion.Status = PluginStatus.Removed;
+            await File.WriteAllTextAsync(PluginConfigFile, JsonSerializer.Serialize(plugins));
+        }
+        await SavePluginsConfig(fileStream, plugins);
+        return plugin;
+    }
+
+    public static async Task<List<PluginConfig>> ListPluginsAsync(bool enabledOnly = false, bool disabledOnly = false, PluginType? type = null)
+    {
+        await using FileStream fileStream = new(PluginConfigFile, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+        List<PluginConfig> plugins = await LoadPluginsConfigAsync(fileStream);
+        //TODO: filter plugins
+        return plugins;
+    }
+
+    //Restore plugin config from .bak file generated at broker startup which contains the running plugin configuration
+    public static async Task<List<PluginConfig>> RestorePluginConfigAsync()
+    {
+        File.Copy(PluginConfigFile, PluginConfigBackupFile);
+        await using FileStream fileStream = new(PluginConfigFile, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+        List<PluginConfig> plugins = await LoadPluginsConfigAsync(fileStream);
+        return plugins;
+    }
+
+    public static async Task<PluginConfig> GetPluginConfigAsync(Guid pluginId)
+    {
+        await using FileStream fileStream = new(PluginConfigFile, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+        List<PluginConfig> plugins = await LoadPluginsConfigAsync(fileStream);
+        return plugins.Find(p => p.Id == pluginId);
+    }
+
+    public static async Task<PluginConfig> GetPluginInfoAsync(string pluginPath)
+    {
+        if (!Path.Exists(pluginPath))
+            throw new FileNotFoundException($"Plugin {pluginPath} not found");
+        //TODO: unzip plugin
+        //TODO: read plugin metadata
+        PluginManifest manifest = new();
+        PluginStatus status = await GetPluginConfigAsync(manifest.Id) != null ? PluginStatus.Enabled : PluginStatus.None;
+        return new PluginConfig
+        {
+            Id = Guid.NewGuid(),
+            Name = manifest.Name,
+            Author = manifest.Author,
+            Description = manifest.Description,
+            Type = manifest.Type,
+            EntryPoint = manifest.EntryPoint,
+            Versions =
+            [
+                new PluginVersionInfo
+                {
+                    Version = manifest.Version,
+                    Status = status
+                }
+            ]
+        };
+    }
+
+    public static async Task<Guid> GetPluginIdByNameAsync(string pluginName)
+    {
+        await using FileStream fileStream = new(PluginConfigFile, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+        List<PluginConfig> plugins = await LoadPluginsConfigAsync(fileStream);
+        PluginConfig plugin = plugins.Find(p => p.Name == pluginName)
+            ?? throw new KeyNotFoundException($"Plugin {pluginName} not found");
+        return plugin.Id;
+    }
+
+    //Broker management
+
+    public static async Task<string> GetBrokerStatusAsync()
     {
         throw new NotImplementedException();
     }
 
-    public Task<Guid> GetPluginIdByNameAsync(string pluginName)
+    public static async Task RestartBrokerAsync()
     {
         throw new NotImplementedException();
     }
 
-    public Task InstallPluginAsync(string pluginPath)
+    public static async Task StartBrokerAsync()
     {
         throw new NotImplementedException();
     }
 
-    public Task<IList<PluginConfig>> ListPluginsAsync(bool enabledOnly = false, bool disabledOnly = false, PluginType? type = null)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<string> ResetPluginConfigAsync(bool dryRun = false)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task RestartBrokerAsync()
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task StartBrokerAsync()
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task StopBrokerAsync()
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task UninstallPluginAsync(Guid pluginId, Version version = null)
+    public static async Task StopBrokerAsync()
     {
         throw new NotImplementedException();
     }
